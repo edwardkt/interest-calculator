e59201d5cee448e23b686b19e5e1a6e4fbed7ad8
Edward Tam <edwardkt@usc.edu>
2014-08-30 09:11:09 +0000

    added all diff days

--- interest.py
+++ interest.py
@@ -3,9 +3,20 @@ import datetime
 print '***********************************************************************'
 print '******************EDWARDS INTEREST CALCULATOR**************************'
 print '***********************************************************************'
+
+# Get todays date
 today = datetime.date.today()
-someday = datetime.date(2011, 8, 15)
-diff = today - someday
-print 'Interest rate for Unsub loan is 6.8% taken on 8/15/2011' 
+
+# Figure out how many days have passed
+unsub_2011 = datetime.date(2011, 8, 15)
+diff_2011 = today - unsub_2011
+unsub_2012 = datetime.date(2012, 8, 17)
+diff_2012 = today - unsub_2012
+unsub_2013 = datetime.date(2013, 8, 16)
+diff_2013 = today - unsub_2013 
+
+# Print outputs
+print 'Interest rate for Unsub loan is 6.8% taken on 8/15/2011'
 print 'Interest rate for Unsub loan is 6.8% taken on 8/17/2012'
 print 'Interest rate for Unsub loan is 3.86% taken on 8/16/2013'
+
