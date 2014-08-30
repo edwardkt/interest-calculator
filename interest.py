fc881ae3283ca0c49ad0ee22da9ca41f3925e899
Edward Tam <edwardkt@usc.edu>
2014-08-30 08:50:42 +0000

    calculated days of interest

--- interest.py
+++ interest.py
@@ -1,2 +1,9 @@
+import datetime
+
 print 'EDWARDS INTEREST CALCULATOR!'
 print '*******************************'
+
+today = datetime.date.today()
+someday = datetime.date(2011, 8, 15)
+diff = today - someday
+print diff.days
