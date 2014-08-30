031ec3dbc0b7f8b98c3ffd409ece3cdbacd6f90d
Edward Tam <edwardkt@usc.edu>
2014-08-30 09:04:33 +0000

    added more comments to program

--- interest.py
+++ interest.py
@@ -1,9 +1,11 @@
 import datetime
 
-print 'EDWARDS INTEREST CALCULATOR!'
-print '*******************************'
-
+print '***********************************************************************'
+print '******************EDWARDS INTEREST CALCULATOR**************************'
+print '***********************************************************************'
 today = datetime.date.today()
 someday = datetime.date(2011, 8, 15)
 diff = today - someday
-print diff.days
+print 'Interest rate for Unsub loan is 6.8% taken on 8/15/2011' 
+print 'Interest rate for Unsub loan is 6.8% taken on 8/17/2012'
+print 'Interest rate for Unsub loan is 3.86% taken on 8/16/2013'
